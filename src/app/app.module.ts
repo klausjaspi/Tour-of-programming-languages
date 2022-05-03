8616e209bf6f7be04a7f6cb30a8ed96537993f1a
klausjaspi <klaus.jaspi@gmail.com>
2022-05-03 15:09:35 +0300

    Added search, adding & deleting language functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { LanguageSearchComponent } from './language-search/language-search.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { InMemoryDataService } from './in-memory-data.service';
     ProgrammingLanguagesComponent,
     LanguageDetailsComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    LanguageSearchComponent
   ],
   imports: [
     BrowserModule,
